a7734cd837b7ecce267ed3cd826e42944ea2f45d
Noman Shamim <nomanshamim2004@gmail.com>
2024-02-07 22:38:56 +0500

    done

--- src/Form/SignUpForm.jsx
+++ src/Form/SignUpForm.jsx
@@ -1,11 +1,63 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import './Form.css';
 import { Button, Checkbox, Form, Input } from 'antd';
 import { Link } from 'react-router-dom';
+import { auth, GoogleAuthProvider, signInWithPopup } from '../Confiq/Confiq'
+import { addDoc, collection, db } from '../Confiq/Confiq';
+
 
 const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
+
+function googlebtn() {
+
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+        .then(async (result) => {
+
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            const user = result.user;
+            Swal.fire({
+                title: "Good job!",
+                text: "Signed in successfully",
+                icon: "success"
+            });
+
+            try {
+                const docRef = await addDoc(collection(db, "users"), {
+                    Name: user.displayName,
+                    Email: user.email,
+                    Time: new Date().toLocaleString()
+                });
+                console.log("Document written with ID: ", docRef.id);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+
+        }).catch((error) => {
+
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: errorCode,
+            });
+
+            const email = error.customData.email;
+
+            const credential = GoogleAuthProvider.credentialFromError(error);
+
+        });
+
+}
+
 const App = ({ SupFunc }) => (
     <Form
         name="basic"
@@ -76,7 +128,7 @@ const App = ({ SupFunc }) => (
             <Button id='button' htmlType="submit">
                 SignUp
             </Button>
-            <Button id="Google">Sign in with Google</Button>
+            <Button id="Google" onClick={googlebtn}>Sign in with Google</Button>
         </Form.Item>
         <p id='para'>Already have an account ? <Link id='link' to={'/login'}>Login</Link></p>
     </Form>
